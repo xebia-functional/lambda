0d44b64f41196809fca0cc809779429441406e75
Paul LaCrosse <paul.lacrosse@consultant.com>
2024-01-03 16:29:35 -0500

    justfile works hashing works

--- rust/https-a/src/main.rs
+++ rust/https-a/src/main.rs
@@ -1,16 +1,32 @@
+// use aws_sdk_dynamodb::Client; //, Error};
 use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, Response};
 
-/// This is the main body for the function.
-/// Write your code inside it.
-/// There are some code example in the following URLs:
 /// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
 async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
+    // let shared_config = aws_config::load_from_env().await;
+    // let client = Client::new(&shared_config);
+    // let req = client.list_tables().limit(10);
+    // let resp = req.send().await?;
+    // println!("Current DynamoDB tables: {:?}", resp.table_names);
+    // let message = format!("Current DynamoDB tables: {:?}", resp.table_names);
+
     // Extract some useful information from the request
     let who = event
         .query_string_parameters_ref()
         .and_then(|params| params.first("name"))
         .unwrap_or("world");
-    let message = format!("Hello {who}, this is a silly AWS Lambda HTTP request");
+
+    use sha3::{Digest, Sha3_512};
+
+    let mut hasher = Sha3_512::new();
+    let data = b"Hello world!";
+    hasher.update(data);
+    // `update` can be called repeatedly and is generic over `AsRef<[u8]>`
+    hasher.update("String data");
+    // Note that calling `finalize()` consumes hasher
+    let hash = hasher.finalize();
+
+    let message = format!("Hello {who}, AWS Lambda HTTP {:?}", hash);
 
     // Return something that implements IntoResponse.
     // It will be serialized to the right response event automatically by the runtime
