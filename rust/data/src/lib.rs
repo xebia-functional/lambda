dd156d4cdf89da391ac1c7c1ac763a406cf7e500
Paul LaCrosse <paul.lacrosse@xebia.com>
2024-01-05 05:55:07 -0500

    wip dynamodb insert

--- rust/data/src/lib.rs
+++ rust/data/src/lib.rs
@@ -1,10 +1,10 @@
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize, Send)]
 pub struct Datum {
-    pub uuid: Uuid,
-    pub doc: String,
-    pub hashes: u16,
-    pub hash: Option<String>,
+	pub uuid: Uuid,
+	pub doc: String,
+	pub hashes: u16,
+	pub hash: Option<String>,
 }
--- rust/https-a/src/main.rs
+++ rust/https-a/src/main.rs
@@ -1,12 +1,15 @@
 use data::Datum;
 
-// use aws_sdk_dynamodb::Client; //, Error};
+use aws_sdk_dynamodb::Client;
 use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, Response};
 use rand::distributions::Alphanumeric;
 use rand::{thread_rng, Rng};
-use serde_json;
+// use serde::{Deserialize, Serialize};
+use serde_dynamo::to_attribute_value;
 use sha3::{Digest, Sha3_512};
+use tracing::info;
 use uuid::Uuid;
+// use serde_json;
 
 fn read_param<T: std::str::FromStr>(event: &Request, name: &str, dflt: T) -> T {
 	event
@@ -16,21 +19,12 @@ fn read_param<T: std::str::FromStr>(event: &Request, name: &str, dflt: T) -> T {
 		.unwrap_or(dflt)
 }
 
-/// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
-async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
-	// let shared_config = aws_config::load_from_env().await;
-	// let client = Client::new(&shared_config);
-	// let req = client.list_tables().limit(10);
-	// let resp = req.send().await?;
-	// println!("Current DynamoDB tables: {:?}", resp.table_names);
-	// let message = format!("Current DynamoDB tables: {:?}", resp.table_names);
-
-	// Extract some useful information from the request
-	//
-	//
+async fn handle_request(db_client: &Client, event: Request) -> Result<Response<Body>, Error> {
 	let chars = read_param(&event, "chars", 1024usize);
 	let hashes = read_param(&event, "hashes", 100u16);
 	let msgs = read_param(&event, "msgs", 64usize);
+
+	// info!(payload = %s, "JSON Payload received");
 
 	// (0..msgs).
 
@@ -48,13 +42,16 @@ async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
 			.collect()
 	}));
 
-	let msg = serde_json::to_string(&Datum {
+	let datum = Datum {
 		uuid: Uuid::new_v4(),
 		doc,
 		hashes,
 		hash,
-	})
-	.unwrap();
+	};
+
+	let msg = serde_json::to_string(&datum).unwrap();
+
+	// add_datum(db_client, datum.clone(), "Datum").await?;
 
 	// Return something that implements IntoResponse.
 	// It will be serialized to the right response event automatically by the runtime
@@ -76,5 +73,35 @@ async fn main() -> Result<(), Error> {
 		.without_time()
 		.init();
 
-	run(service_fn(function_handler)).await
+	//Get config from environment.
+	let config = aws_config::load_from_env().await;
+	//Create the DynamoDB client.
+	let client = Client::new(&config);
+
+	run(service_fn(|event: Request| async {
+		handle_request(&client, event).await
+	}))
+	.await
+}
+
+// TODO: Add a datum to a table.
+pub async fn add_datum(client: &Client, d: Datum, table: &str) -> Result<(), Error> {
+	// let uuid_av = to_attribute_value(d.uuid)?;
+	// let doc_av = to_attribute_value(d.doc)?;
+	// let hashes_av = to_attribute_value(d.hashes)?;
+	// let hash_av = to_attribute_value(d.hash)?;
+
+	// let request = client
+	// .put_item()
+	// .table_name(table)
+	// .item("uuid", uuid_av)
+	// .item("doc", doc_av)
+	// .item("hashes", hashes_av)
+	// .item("hash", hash_av);
+
+	// info!("adding item to DynamoDB");
+
+	// let _resp = request.send().await?;
+
+	Ok(())
 }
